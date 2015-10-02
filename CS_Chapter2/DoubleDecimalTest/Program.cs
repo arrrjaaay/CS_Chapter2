bdc7a9292508e53e6aa06ae36ce9ca5c5cac4797
RJ Santos <rjs4nt0s@yahoo.com>
2015-10-02 09:52:21 -0400

    v1
    finished double decimal and room

--- CS_Chapter2/DoubleDecimalTest/Program.cs
+++ CS_Chapter2/DoubleDecimalTest/Program.cs
@@ -9,6 +9,10 @@ namespace DoubleDecimalTest
     {
         static void Main(string[] args)
         {
+            double number = 4.20;
+            Console.WriteLine(number);
+            decimal number2 = 4.20m;
+            Console.WriteLine(number2);
         }
     }
 }
--- CS_Chapter2/Room/Program.cs
+++ CS_Chapter2/Room/Program.cs
@@ -9,6 +9,10 @@ namespace Room
     {
         static void Main(string[] args)
         {
+            double length = 10;
+            double width = 25;
+            double area = length * width;
+            Console.WriteLine("The floor space is {0} square feet.", area); 
         }
     }
 }
