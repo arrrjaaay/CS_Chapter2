4d37b13e0b80eacd6a50d2aa80f75ab71f115d72
RJ Santos <rjs4nt0s@yahoo.com>
2015-10-05 11:33:01 -0400

    finished carpet and yards

--- CS_Chapter2/Carpet/Program.cs
+++ CS_Chapter2/Carpet/Program.cs
@@ -9,6 +9,14 @@ namespace Carpet
     {
         static void Main(string[] args)
         {
+            double length = 10.5;
+            double width = 25.5;
+            double area = length * width;
+            double price = 2.50;
+            string carpetprice;
+            carpetprice = price.ToString("F");
+            double totalprice = area * price;
+            Console.WriteLine("The cost per square foot is ${0}. The cost to carpet this room is ${1}.", carpetprice, totalprice);
         }
     }
 }
--- CS_Chapter2/Yards/Program.cs
+++ CS_Chapter2/Yards/Program.cs
@@ -9,6 +9,14 @@ namespace Yards
     {
         static void Main(string[] args)
         {
+            double length = 10.5;
+            double width = 25.5;
+            double area = (length / 3) * (width / 3);
+            double price = 5.00;
+            string carpetprice;
+            carpetprice = price.ToString("F");
+            double totalprice = area * price;
+            Console.WriteLine("The cost per square yard is ${0}. The cost to carpet this room is ${1}.", carpetprice, totalprice);
         }
     }
 }
