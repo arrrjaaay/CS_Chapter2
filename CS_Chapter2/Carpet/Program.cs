08a44564e1ae81478940096304ccdd349d9d52d1
RJ Santos <rjs4nt0s@yahoo.com>
2015-10-23 10:50:05 -0400

    actually finished

--- CS_Chapter2/Carpet/Program.cs
+++ CS_Chapter2/Carpet/Program.cs
@@ -9,14 +9,19 @@ namespace Carpet
     {
         static void Main(string[] args)
         {
-            double length = 10.5;
+            double length = 10.25;
             double width = 25.5;
             double area = length * width;
+
             double price = 2.50;
             string carpetprice;
-            carpetprice = price.ToString("F");
+            carpetprice = price.ToString("C");
+
             double totalprice = area * price;
-            Console.WriteLine("The cost per square foot is ${0}. The cost to carpet this room is ${1}.", carpetprice, totalprice);
+            string total;
+            total = totalprice.ToString("C");
+            
+            Console.WriteLine("The cost per square foot is {0}. The cost to carpet this room is {1}.", carpetprice, total);
         }
     }
 }
